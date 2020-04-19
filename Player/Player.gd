abb65a2a601cd948d1c585ffbdd90e1c53f2c31b
Christoph Schroeder <call-schroedi@web.de>
2020-04-19 08:22:52 +0200

    Player input fixes

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody
 
-export var ACCELERATION = 5
-export var MAX_SPEED = 2
-export var ROLL_SPEED = 3
-export var FRICTION = 5
+export var ACCELERATION = 5.0
+export var MAX_SPEED = 2.0
+export var ROLL_SPEED = 3.0
+export var FRICTION = 5.0
 
 const ANIM_LIGHT = 0
 const ANIM_SEMILIGHT = 1
@@ -41,19 +41,6 @@ func _ready():
 	weapon.facing_dir = facing_dir
 	Globals.player_weapon = weapon
 
-func _physics_process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
-		ROLL:
-			roll_state(delta)
-		ATTACK:
-			attack_state(delta)
-
-func move_state(delta):
-	process_input(delta)
-	move(delta)
-	
 func process_input(delta):
 	var input_vector = Vector3.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -74,25 +61,38 @@ func process_input(delta):
 	
 	animationTree.set("parameters/WalkIdle/blend_amount", 1-velocity.length())
 	
-	if Input.is_action_just_pressed("roll"):
-		animationTree.set("parameters/RollShot/active", true)
-		$RollTimer.wait_time = animationPlayer.get_animation("Roll").length
-		$RollTimer.start()
-		state = ROLL
-	
-	if Input.is_action_just_pressed("attack"):
-		attack_animation_started()
-		state = ATTACK
-		pass
+
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			if Input.is_action_just_pressed("roll"):
+				animationTree.set("parameters/RollShot/active", true)
+				$RollTimer.wait_time = animationPlayer.get_animation("Roll").length
+				$RollTimer.start()
+				state = ROLL
+			
+			if Input.is_action_just_pressed("attack"):
+				attack_animation_started()
+				state = ATTACK
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+	process_input(delta)
+	move(delta)
 
 func roll_state(delta):
 	velocity = facing_dir * ROLL_SPEED
+	process_input(delta)
 	move(delta)
 
 func attack_state(delta):
-	#process_input(delta)
+	process_input(delta)
 	move(delta)
-	
 
 func move(_delta):
 	velocity = move_and_slide_with_snap(velocity, Vector3.DOWN * 100, Vector3.UP, true, 4, PI/2.0)
@@ -107,22 +107,16 @@ func attack_animation_started():
 	# set attack type
 	animationTree.set("parameters/Attack/current", curre_attack_type)
 	animationTree.set("parameters/Seek/seek_position", 0)
-	# start transition
-	#animationTree.set("parameters/AttackAdd/amount", 1.0)
 	weapon.start_attack()
-	print("dmg start")
 
 func attack_animation_finished():
 	animationTree.set("parameters/AttackAdd/amount", 0)
 	state = MOVE
 	weapon.end_attack()
-	print("dmg end")
 
 func _on_RollTimer_timeout() -> void:
 	roll_animation_finished()
-	pass # Replace with function body.
 
 
 func _on_AttackTimer_timeout() -> void:
 	attack_animation_finished()
-	pass # Replace with function body.
